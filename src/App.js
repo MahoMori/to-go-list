a93fd330e0a8ea0bfcbd130f08feaf69202296c4
MahoMori <tr27e1104@gmail.com>
2021-12-18 15:43:33 -0800

    tried but failed adding signup feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 
+// import SignUp from "./components/signup/signup.component.uname";
+// import SignupContainer from "./components/signup/signup.container";
+
 import MainPage from "./components/main-page";
 
 function App() {
   return (
     <div className="App">
+      {/* <SignUp /> */}
+      {/* <SignupContainer /> */}
       <MainPage />
     </div>
   );
