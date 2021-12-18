9f7d68cecca7965534592355fb851d85ab41e7b1
MahoMori <tr27e1104@gmail.com>
2021-12-18 12:26:42 -0800

    tried using useParams

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
+
 import MainPage from "./components/main-page";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
-      {/* <Routes>
-        <Route exact path="/" component={MainPage} />
-      </Routes> */}
+      <Routes>
+        <Route exact path="/main/:id" component={MainPage} />
+      </Routes>
     </div>
   );
 }
--- src/components/name-tab/NameTabPanel.jsx
+++ src/components/name-tab/NameTabPanel.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Route } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
@@ -37,6 +37,7 @@ TabPanel.propTypes = {
 };
 
 export default function NameTabPanel() {
+  const { id } = useParams();
   const users = React.useContext(usersContext);
 
   const matches = useMediaQuery("(max-width:600px)");
@@ -76,8 +77,6 @@ export default function NameTabPanel() {
                 {users.map((user) => (
                   <Tab label={user} />
                 ))}
-                {/* <Tab label="Maho" />
-                <Tab label="Tori" /> */}
               </Tabs>
             </NavBar>
           </Grid>
@@ -95,21 +94,35 @@ export default function NameTabPanel() {
                 height: "calc(100vh - 64px)",
               }}
             >
+              {/* {users.map((user) => {
+                user === id && (
+                  <Link to={`/${user}`}>
+                    <Tab label={user} />
+                  </Link>
+                );
+              })} */}
               {users.map((user) => (
                 <Tab label={user} />
               ))}
-              {/* <Tab label="Maho" />
-              <Tab label="Tori" /> */}
             </Tabs>
           </Grid>
         )}
 
         <Grid item md={10} sx={matches ? { width: "100%" } : { width: "85%" }}>
-          {users.map((user, i) => (
+          {users.map((user, i) => {
+            user === id && (
+              <Link to={`/${user}`}>
+                <TabPanel value={value} index={i}>
+                  <ListTabPanel nameOfCreator={user} />
+                </TabPanel>
+              </Link>
+            );
+          })}
+          {/* {users.map((user, i) => (
             <TabPanel value={value} index={i}>
               <ListTabPanel nameOfCreator={user} />
             </TabPanel>
-          ))}
+          ))} */}
         </Grid>
       </Grid>
     </Box>
